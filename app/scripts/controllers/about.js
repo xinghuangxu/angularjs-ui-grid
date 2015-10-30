84f30fff0aef3d0b32449fb4ed6343a3504b3c2d
Xinghuang Leon Xu <xinghuangxu@gmail.com>
2015-10-29 23:31:18 -0500

    initial app creation

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uiGridApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the uiGridApp
+ */
+angular.module('uiGridApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uiGridApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uiGridApp
+ */
+angular.module('uiGridApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
